1d94c7aa726eb6c282332eba8d9a9c23bb5a9adb
dubaokun <dubaokun@xiaomi.com>
2017-09-25 11:28:55 +0800

    update file structure

--- e2e/test.js
+++ e2e/test.js
@@ -1,11 +1,13 @@
 
 module.exports = {
   'Demo test' : function (browser) {
-     console.log(browser.globals);
-    
-  },
-  after : function(browser) {
-      browser.end();
+    browser
+      .url("http://www.baidu.com")
+      .waitForElementVisible('body', 1000)
+      .setValue('#index-kw',"面向对象")
+      .click('#index-bn')
+      .assert.containsText('body', '面向对象')
+       .saveScreenshot('./test.png')
+      .end();
   }
-
 };
--- sum.js
+++
@@ -1,9 +0,0 @@
-
-function sum(a,b){
-  if(a>0){
-    return a+b;
-  }else{
-    return a-b;
-  }
-}
-module.exports = sum;
